41c2ddf9fcede7ff917d0bb87b8bc883a63269dc
rafaelsous <rafaelsousa.souza@gmail.com>
2020-07-28 11:54:31 -0300

    Change blowing animation

--- src/screens/Animations/Blowing/index.tsx
+++ src/screens/Animations/Blowing/index.tsx
@@ -1,24 +1,138 @@
 import React, { useRef, useState } from "react";
-import { View, StyleSheet, TouchableOpacity, Animated } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+  Dimensions,
+  Text,
+} from "react-native";
 import { AntDesign as Icon } from "@expo/vector-icons";
+import Constants from "expo-constants";
+import { StatusBar } from "expo-status-bar";
 
 const CIRCLE_SIZE = 100;
+const DURATION = 1000;
+const TEXT_DURATION = DURATION * 0.8;
+const { width } = Dimensions.get("window");
 
 interface CircleProps {
-  onPress: () => void;
+  index: number;
+  quotes: { quote: string; author: string }[];
   animatedValue: Animated.Value;
+  animatedValue2: Animated.Value;
+  onPress: () => void;
 }
 
-const Circle = ({ onPress, animatedValue }: CircleProps) => {
+const quotes = [
+  {
+    quote:
+      "For the things we have to learn before we can do them, we learn by doing them.",
+    author: "Aristotle, The Nicomachean Ethics",
+  },
+  {
+    quote: "The fastest way to build an app.",
+    author: "The Expo Team",
+  },
+  {
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
+  },
+  {
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    author: "Steve Jobs",
+  },
+  {
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
+  },
+  // {
+  //   quote:
+  //     "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+  //   author: "Oprah Winfrey",
+  // },
+  // {
+  //   quote:
+  //     "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+  //   author: "James Cameron",
+  // },
+  // {
+  //   quote: "Life is what happens when you're busy making other plans.",
+  //   author: "John Lennon",
+  // },
+];
+
+const colors = [
+  {
+    initialBgColor: "goldenrod",
+    bgColor: "#222",
+    nextBgColor: "#222",
+  },
+  {
+    initialBgColor: "goldenrod",
+    bgColor: "#222",
+    nextBgColor: "yellowgreen",
+  },
+  {
+    initialBgColor: "#222",
+    bgColor: "yellowgreen",
+    nextBgColor: "midnightblue",
+  },
+  {
+    initialBgColor: "yellowgreen",
+    bgColor: "midnightblue",
+    nextBgColor: "turquoise",
+  },
+  {
+    initialBgColor: "midnightblue",
+    bgColor: "turquoise",
+    nextBgColor: "goldenrod",
+  },
+  {
+    initialBgColor: "turquoise",
+    bgColor: "goldenrod",
+    nextBgColor: "#222",
+  },
+];
+
+const Circle = ({
+  index,
+  animatedValue,
+  animatedValue2,
+  onPress,
+}: CircleProps) => {
+  const { initialBgColor, bgColor, nextBgColor } = colors[index];
   const inputRange = [0, 0.001, 0.5, 0.501, 1];
-  const containerBg = animatedValue.interpolate({
+  const AnimatedIcon = Animated.createAnimatedComponent(Icon);
+
+  const backgroundColor = animatedValue2.interpolate({
     inputRange,
-    outputRange: ["gold", "gold", "gold", "#444", "#444"],
+    outputRange: [
+      initialBgColor,
+      initialBgColor,
+      initialBgColor,
+      bgColor,
+      bgColor,
+    ],
   });
 
-  const circleBg = animatedValue.interpolate({
-    inputRange,
-    outputRange: ["#444", "#444", "#444", "gold", "gold"],
+  const dotBgColor = animatedValue2.interpolate({
+    inputRange: [0, 0.001, 0.5, 0.501, 0.9, 1],
+    outputRange: [
+      bgColor,
+      bgColor,
+      bgColor,
+      initialBgColor,
+      initialBgColor,
+      nextBgColor,
+    ],
   });
 
   return (
@@ -26,32 +140,32 @@ const Circle = ({ onPress, animatedValue }: CircleProps) => {
       style={[
         StyleSheet.absoluteFillObject,
         styles.circleContainer,
-        { backgroundColor: containerBg },
+        { backgroundColor },
       ]}
     >
       <Animated.View
         style={[
           styles.circle,
           {
-            backgroundColor: circleBg,
+            backgroundColor: dotBgColor,
             transform: [
               {
-                perspective: 400,
+                perspective: 200,
               },
               {
-                rotateY: animatedValue.interpolate({
+                rotateY: animatedValue2.interpolate({
                   inputRange: [0, 0.5, 1],
                   outputRange: ["0deg", "-90deg", "-180deg"],
                 }),
               },
               {
-                scale: animatedValue.interpolate({
+                scale: animatedValue2.interpolate({
                   inputRange: [0, 0.5, 1],
-                  outputRange: [1, 8, 1],
+                  outputRange: [1, 6, 1],
                 }),
               },
               {
-                translateX: animatedValue.interpolate({
+                translateX: animatedValue2.interpolate({
                   inputRange: [0, 0.5, 1],
                   outputRange: [0, 0.5, 0],
                 }),
@@ -61,9 +175,33 @@ const Circle = ({ onPress, animatedValue }: CircleProps) => {
         ]}
       >
         <TouchableOpacity {...{ onPress }}>
-          <View style={[styles.circle, styles.circleButton]}>
-            <Icon name="arrowright" size={28} color="white" />
-          </View>
+          <Animated.View
+            style={[
+              styles.button,
+              {
+                transform: [
+                  {
+                    scale: animatedValue.interpolate({
+                      inputRange: [0, 0.05, 0.5, 1],
+                      outputRange: [1, 0, 0, 1],
+                    }),
+                  },
+                  {
+                    rotateY: animatedValue.interpolate({
+                      inputRange: [0, 0.5, 0.9, 1],
+                      outputRange: ["0deg", "180deg", "180deg", "180deg"],
+                    }),
+                  },
+                ],
+                opacity: animatedValue.interpolate({
+                  inputRange: [0, 0.5, 0.9, 1],
+                  outputRange: [1, 0, 0, 1],
+                }),
+              },
+            ]}
+          >
+            <AnimatedIcon name="right" size={20} color={"white"} />
+          </Animated.View>
         </TouchableOpacity>
       </Animated.View>
     </Animated.View>
@@ -72,23 +210,96 @@ const Circle = ({ onPress, animatedValue }: CircleProps) => {
 
 const Blowing: React.FC = () => {
   const animatedValue = useRef(new Animated.Value(0)).current;
-  const animation = (toValue: number) =>
-    Animated.timing(animatedValue, {
-      toValue,
-      duration: 3000,
-      useNativeDriver: false,
-    });
-
+  const animatedValue2 = useRef(new Animated.Value(0)).current;
+  const sliderAnimatedValue = useRef(new Animated.Value(0)).current;
+  const inputRange = [...Array(quotes.length).keys()];
   const [index, setIndex] = useState(0);
 
+  const animation = (i: number) =>
+    Animated.parallel([
+      Animated.timing(sliderAnimatedValue, {
+        toValue: i,
+        duration: TEXT_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue2, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: false,
+      }),
+    ]);
+
   const onPress = () => {
-    setIndex(index === 1 ? 0 : 1);
-    animation(index === 1 ? 0 : 1).start();
+    animatedValue.setValue(0);
+    animatedValue2.setValue(0);
+
+    animation((index + 1) % colors.length).start();
+    setIndex((index + 1) % colors.length);
   };
 
   return (
     <View style={styles.container}>
-      <Circle {...{ onPress }} {...{ animatedValue }} />
+      <StatusBar hidden translucent backgroundColor="transparent" />
+      <Circle
+        {...{ index }}
+        {...{ quotes }}
+        {...{ animatedValue }}
+        {...{ animatedValue2 }}
+        {...{ onPress }}
+      />
+
+      <Animated.View
+        style={{
+          flexDirection: "row",
+          transform: [
+            {
+              translateX: sliderAnimatedValue.interpolate({
+                inputRange,
+                outputRange: quotes.map((_, i) => -i * width * 2),
+              }),
+            },
+          ],
+          opacity: sliderAnimatedValue.interpolate({
+            inputRange: [...Array(quotes.length * 2 + 1).keys()].map(
+              (i) => i / 2
+            ),
+            outputRange: [...Array(quotes.length * 2 + 1).keys()].map((i) =>
+              i % 2 === 0 ? 1 : 0
+            ),
+          }),
+        }}
+      >
+        {quotes.slice(0, colors.length).map(({ quote, author }, i) => {
+          return (
+            <View style={{ paddingRight: width, width: width * 2 }} key={i}>
+              <Text
+                style={[styles.paragraph, { color: colors[i].nextBgColor }]}
+              >
+                {quote}
+              </Text>
+              <Text
+                style={[
+                  styles.paragraph,
+                  {
+                    color: colors[i].nextBgColor,
+                    fontSize: 10,
+                    fontWeight: "normal",
+                    textAlign: "right",
+                    opacity: 0.8,
+                  },
+                ]}
+              >
+                ______ {author}
+              </Text>
+            </View>
+          );
+        })}
+      </Animated.View>
     </View>
   );
 };
@@ -97,25 +308,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "flex-start",
-  },
-  circleButton: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "transparent",
+    paddingTop: 100,
   },
   circleContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "flex-end",
+    paddingTop: Constants.statusBarHeight,
     padding: 8,
-    paddingBottom: 100,
-    backgroundColor: "gold",
+    paddingBottom: 50,
+  },
+  button: {
+    width: CIRCLE_SIZE,
+    height: CIRCLE_SIZE,
+    borderRadius: CIRCLE_SIZE / 2,
+    alignItems: "center",
+    justifyContent: "center",
   },
   circle: {
     width: CIRCLE_SIZE,
     height: CIRCLE_SIZE,
     borderRadius: CIRCLE_SIZE / 2,
-    backgroundColor: "#444",
+    backgroundColor: "turquoise",
+  },
+  paragraph: {
+    margin: 12,
+    fontSize: 24,
+    textAlign: "center",
+    fontFamily: "Roboto",
+    color: "white",
   },
 });
 
